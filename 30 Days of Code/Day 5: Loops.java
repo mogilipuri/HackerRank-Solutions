25eb9b09251cfaa97e7b2478c7b9e43220b76615
mogilipuri <40380106+mogilipuri@users.noreply.github.com>
2018-10-06 15:52:52 +0530

    Create Day 5: Loops.java

---
+++ 30 Days of Code/Day 5: Loops.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i=1;i<=10;i++)
+            {
+            System.out.print(N);
+            System.out.print(" x ");
+            System.out.print(i);
+            System.out.print(" = ");
+            System.out.print(N*i);
+             System.out.println();           
+
+        }
+    }
+}
