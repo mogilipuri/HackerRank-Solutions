1f382fa7bc765b38ef3a005b8114b800857714f7
mogilipuri <40380106+mogilipuri@users.noreply.github.com>
2018-10-06 15:00:49 +0530

    Create Python If-Else.py

---
+++ Python/Python If-Else.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    if n%2==1:
+        print("Weird")
+    else:
+        if n<=5:
+            print("Not Weird")
+        elif n<=20:
+            print("Weird")
+        else:
+            print("Not Weird")
